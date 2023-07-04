c9544799de5bddb757bcd0c4c1722bf643196c7b
Li, ChungHao <chung@stemworldeducationalservices.com>
2023-07-04 14:02:55 -0400

    Networked grabbing - Remote Procedure Calls - RPCs

--- Assets/IRONHEAD Games/Scripts/NetworkedGrabbing.cs
+++ Assets/IRONHEAD Games/Scripts/NetworkedGrabbing.cs
@@ -8,6 +8,8 @@ public class NetworkedGrabbing : MonoBehaviourPunCallbacks, IPunOwnershipCallbac
 {
     PhotonView m_PhotonView;
 
+    Rigidbody m_Rigidbody;
+    bool m_isBeingHeld = false;
     private void Awake()
     {
         m_PhotonView = GetComponent<PhotonView>();
@@ -15,13 +17,23 @@ public class NetworkedGrabbing : MonoBehaviourPunCallbacks, IPunOwnershipCallbac
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Rigidbody=GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (m_isBeingHeld)
+        {
+            //gun is now being held by someone
+            m_Rigidbody.isKinematic = true;
+            gameObject.layer = 11;
+        }
+        else
+        {
+            m_Rigidbody.isKinematic = false;
+            gameObject.layer = 9;
+        }
     }
 
     private void TransferOwnership()
@@ -32,15 +44,30 @@ public class NetworkedGrabbing : MonoBehaviourPunCallbacks, IPunOwnershipCallbac
     public void OnSelectEntered()
     {
         Debug.Log("Grabbed");
-        TransferOwnership();
+        m_PhotonView.RPC("StartNetworkGrabbing", RpcTarget.AllBuffered); //work on everyone in the room including people who join later
+
+        if (m_PhotonView.Owner == PhotonNetwork.LocalPlayer)
+        {
+            Debug.Log("No need to transfer ownership. Already mine");
+        }
+        else 
+        {
+            TransferOwnership();
+        }
     }
     public void OnSelectExited()
     {
         Debug.Log("Released");
+        m_PhotonView.RPC("EndNetworkGrabbing", RpcTarget.AllBuffered);
     }
 
     public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
     {
+        if (targetView != m_PhotonView) //only this script attached target object will be called 
+        {
+            return;
+        }
+
         Debug.Log("Ownership requested for: " + targetView.name + " from: " + requestingPlayer.NickName);
         targetView.TransferOwnership(requestingPlayer);
     }
@@ -53,5 +80,17 @@ public class NetworkedGrabbing : MonoBehaviourPunCallbacks, IPunOwnershipCallbac
     public void OnOwnershipTransferFailed(PhotonView targetView, Player senderOfFailedRequest)
     {
 
+    }
+
+    [PunRPC]
+    public void StartNetworkGrabbing()
+    {
+        m_isBeingHeld = true;
+    }
+
+    [PunRPC]
+    public void EndNetworkGrabbing()
+    {
+        m_isBeingHeld = false;
     }
 }
