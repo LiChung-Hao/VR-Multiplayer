123179984cbf95ad719341b95eff511a8dac9463
Li, ChungHao <chung@stemworldeducationalservices.com>
2023-07-04 12:52:43 -0400

    Networked Grabbing - ownership transfer

--- Assets/IRONHEAD Games/Scripts/NetworkedGrabbing.cs
+++ Assets/IRONHEAD Games/Scripts/NetworkedGrabbing.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class NetworkedGrabbing : MonoBehaviour
+public class NetworkedGrabbing : MonoBehaviourPunCallbacks, IPunOwnershipCallbacks
 {
+    PhotonView m_PhotonView;
+
+    private void Awake()
+    {
+        m_PhotonView = GetComponent<PhotonView>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +24,34 @@ public class NetworkedGrabbing : MonoBehaviour
         
     }
 
+    private void TransferOwnership()
+    {
+        m_PhotonView.RequestOwnership();
+    }
+
     public void OnSelectEntered()
     {
         Debug.Log("Grabbed");
+        TransferOwnership();
     }
     public void OnSelectExited()
     {
         Debug.Log("Released");
     }
 
+    public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
+    {
+        Debug.Log("Ownership requested for: " + targetView.name + " from: " + requestingPlayer.NickName);
+        targetView.TransferOwnership(requestingPlayer);
+    }
+
+    public void OnOwnershipTransfered(PhotonView targetView, Player previousOwner)
+    {
+        Debug.Log("Ownership transferred to: " + targetView.name + " from: " + previousOwner.NickName);
+    }
+
+    public void OnOwnershipTransferFailed(PhotonView targetView, Player senderOfFailedRequest)
+    {
+
+    }
 }
